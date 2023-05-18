07c6f18467f70454b3627431677bc14ea4175ec4
Erick Mendes da Silva <erickmendesweb@gmail.com>
2023-05-18 14:52:17 -0300

    Revert "Redis implementation"

--- src/repositories/event-repository/index.ts
+++ src/repositories/event-repository/index.ts
@@ -1,27 +1,7 @@
-import { Event } from '@prisma/client';
 import { prisma } from '@/config';
-import { redis } from '@/config/redis';
-
-async function getCachedEvent(cacheKey: string): Promise<Event | null> {
-  const cacheEvent = await redis.get(cacheKey);
-  if (cacheEvent) {
-    const event = JSON.parse(cacheEvent);
-    return event;
-  }
-
-  return null;
-}
 
 async function findFirst() {
-  const cacheKey = 'event';
-  const cacheEvent = await getCachedEvent(cacheKey);
-  if (cacheEvent) return cacheEvent;
-
-  const event = await prisma.event.findFirst();
-
-  redis.set(cacheKey, JSON.stringify(event));
-
-  return event;
+  return prisma.event.findFirst();
 }
 
 const eventRepository = {
