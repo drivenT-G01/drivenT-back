cd06c575c0c73d6b5a0eed0d51874a8774062733
Arthur Saito <arthur_saito1998@hotmail.com>
2023-05-18 00:22:28 -0300

    feat: change event repository to redis.Need to be fixed.Not working

--- src/repositories/event-repository/index.ts
+++ src/repositories/event-repository/index.ts
@@ -1,7 +1,24 @@
 import { prisma } from '@/config';
+import { redis } from '@/config/redis';
+
+async function getCachedEvent(cacheKey: string): Promise<Event | null> {
+  const cacheEvent = await redis.get(cacheKey);
+  if (cacheEvent) {
+    const event = JSON.parse(cacheEvent);
+    return event;
+  }
+
+  return null;
+}
 
 async function findFirst() {
-  return prisma.event.findFirst();
+  const cacheKey = 'event';
+  const cacheEvent = await getCachedEvent(cacheKey);
+
+  if (cacheEvent) return cacheEvent;
+  return await prisma.event.findFirst();
+
+  redis.set(cacheKey, JSON.stringify(event));
 }
 
 const eventRepository = {
