a0ce1ce8d3b08095fe8b13646798917a12c9dc87
samuel <shemlouk@gmail.com>
2023-05-23 18:33:56 -0300

    feat: add data formatting to get-activities function

--- src/services/activities-service/index.ts
+++ src/services/activities-service/index.ts
@@ -1,7 +1,28 @@
+import dayjs from 'dayjs';
 import activitiesRepository from '@/repositories/activities-repository';
 
-const getByScheduleId = async (scheduleId: number) => {
-  return activitiesRepository.findManyByScheduleId(scheduleId);
+type FormatedActivity = {
+  id: number;
+  name: string;
+  startsAt: string;
+  endsAt: string;
+  capacity: number;
+  local: string;
+};
+
+const getByScheduleId = async (scheduleId: number): Promise<FormatedActivity[]> => {
+  const activities = await activitiesRepository.findManyByScheduleId(scheduleId);
+
+  return activities.map(({ id, name, startsAt, endsAt, capacity, local }) => {
+    return {
+      id,
+      name,
+      startsAt: dayjs(startsAt).format('HH:mm'),
+      endsAt: dayjs(endsAt).format('HH:mm'),
+      capacity,
+      local,
+    };
+  });
 };
 
 const activitiesService = { getByScheduleId };
