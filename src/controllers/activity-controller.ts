9831917e63a9f2b84684665a6f881607358b958c
Arthur Saito <arthur_saito1998@hotmail.com>
2023-05-23 00:39:11 -0300

    feat: adding activity controller

--- src/controllers/activity-controller.ts
+++ src/controllers/activity-controller.ts
@@ -1,11 +1,13 @@
-import { NextFunction } from 'express';
+import { NextFunction, Response } from 'express';
 // eslint-disable-next-line import/no-unresolved
+import httpStatus from 'http-status';
 import { AuthenticatedRequest } from '@/middlewares';
+import activityService from '@/services/activity-service';
 
-export async function getHotels(req: AuthenticatedRequest, res: Response, next: NextFunction) {
-  const { userId } = req;
-
+export async function getAllActivities(req: AuthenticatedRequest, res: Response, next: NextFunction) {
   try {
+    const activities = await activityService.getAllActivities();
+    return res.status(httpStatus.OK).send(activities);
   } catch (error) {
     next(error);
   }
