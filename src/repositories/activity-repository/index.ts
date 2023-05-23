650a081078f13715d7d15ae91458bd5b725a1233
Arthur Saito <arthur_saito1998@hotmail.com>
2023-05-23 19:29:36 -0300

    fix: adding schedule return on repository

--- src/repositories/activity-repository/index.ts
+++ src/repositories/activity-repository/index.ts
@@ -2,7 +2,7 @@ import { prisma } from '@/config';
 
 async function getAllActivities() {
   return prisma.activity.findMany({
-    include: { ActivityBooking: true },
+    include: { ActivityBooking: true, schedule: true },
   });
 }
 const activityRepository = {
