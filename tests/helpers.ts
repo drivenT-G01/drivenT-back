80c19dd603d34b4756b7f78382c3d2e6cec9da94
Erick Mendes <erickmendesweb@gmail.com>
2023-05-27 15:45:25 -0300

    feat: testes de integração concluidos com a rota post de activity

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -17,6 +17,7 @@ export async function cleanDb() {
   await prisma.room.deleteMany({});
   await prisma.hotel.deleteMany({});
   await prisma.booking.deleteMany({});
+  await prisma.activityBooking.deleteMany({});
   await prisma.activity.deleteMany({});
   await prisma.schedule.deleteMany({});
 }
